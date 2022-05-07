41e218c1604533236bdcf1d1d0c99f0444612c34
BenAidlin <ben.aidlin@gmail.com>
2022-05-07 19:38:35 +0300

    before moving things from remap to start

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 var context;
 var shape = new Object();
 var monsters = [];
+var monsters_images = [];
+var moving_score = new Object();
 var board;
 var score = 0;
 var pac_color;
@@ -10,6 +12,8 @@ var interval;
 var database = [];
 var active_user = "";
 var life = 5;
+var enemy_move_counter = 1;
+
 // settings
 var monsters_amount = 2; // 1-4
 var game_time = 90; // minimum 60
@@ -124,11 +128,9 @@ function StartGame(){
 	Start();
 }
 
-
-function Start() {
+function remap(){
 	board = new Array();
-	//score = 0;
-	pac_color = "yellow";
+	monsters = []	
 	var cnt = 100;
 	var food_remain = 50;
 	var pacman_remain = 1;
@@ -162,6 +164,7 @@ function Start() {
 			}
 		}		
 	}
+	// random monsters
 	for (var k=0;k<monsters_amount;k++){
 		let monster = new Object();
 		let random = Math.random();
@@ -182,14 +185,28 @@ function Start() {
 			monster.j = 9;	
 		}
 		monsters.push(monster);
-		//board[monster.i][monster.j] = 3;
 	}
+	// random moving target
+	var moving_cell = findRandomEmptyCell(board);
+	moving_score.i = moving_cell[0];
+	moving_score.j = moving_cell[1];
+	// random place
 	while (food_remain > 0) {
 		var emptyCell = findRandomEmptyCell(board);
 		board[emptyCell[0]][emptyCell[1]] = 1;
 		food_remain--;
 	}
 	keysDown = {};
+}
+
+function Start() {	
+	moving_score = new Object();
+	monsters_images = [];
+	for(let l=0;l<monsters_amount;l++){
+		let randomint = Math.round(Math.random() *(8) + 1);
+		monsters_images.push("extensions/gamefigures/enemy"+ randomint.toString() + ".png");
+	}
+	remap();
 	addEventListener(
 		"keydown",
 		function(e) {
@@ -204,7 +221,7 @@ function Start() {
 		},
 		false
 	);
-	interval = setInterval(UpdatePosition, 100);
+	interval = setInterval(UpdatePosition, 85);
 }
 
 function findRandomEmptyCell(board) {
@@ -235,28 +252,18 @@ function GetKeyPressed() {
 function Draw() {
 	canvas.width = canvas.width; //clean board
 	lblScore.value = score;
-	lblTime.value = time_elapsed;	
+	lblTime.value = time_elapsed;
+	lblLife.value = life.toString() + " life left";
+	// draw board	
 	for (var i = 0; i < 10; i++) {
 		for (var j = 0; j < 10; j++) {
 			var center = new Object();
 			center.x = i * 60 + 30;
 			center.y = j * 60 + 30;
-			if (board[i][j] == 2) {
-				/* context.beginPath();
-				context.arc(center.x, center.y, 30, 0.15 * Math.PI, 1.85 * Math.PI); // half circle
-				context.lineTo(center.x, center.y);
-				context.fillStyle = pac_color; //color
-				context.fill();
-				context.beginPath();
-				context.arc(center.x + 5, center.y - 15, 5, 0, 2 * Math.PI); // circle
-				context.fillStyle = "white"; //color
-				context.fill(); */
+			if (board[i][j] == 2) {			
 				var img = new Image();
-				img.src = "extensions/gamefigures/pacman" + direction +".png";	
-				//context.scale(0,-1);			
-				context.drawImage(img, center.x - 30, center.y-30)
-				//context.restore();
-
+				img.src = "extensions/gamefigures/pacman" + direction +".png";					
+				context.drawImage(img, center.x - 30, center.y-30)				
 			} else if (board[i][j] == 1) {
 				context.beginPath();
 				context.arc(center.x, center.y, 15, 0, 2 * Math.PI); // circle
@@ -267,20 +274,24 @@ function Draw() {
 				context.rect(center.x - 30, center.y - 30, 60, 60);
 				context.fillStyle = "white"; //color
 				context.fill();
-			}/* else if(board[i][j] == 3){
-				var img2 = new Image();
-				img2.src = "extensions/gamefigures/enemy1.png"
-				context.drawImage(img2, center.x - 30, center.y-30)
-			}*/
+			}
 		}
 	}
-
+	// draw monsters
 	for (let k=0;k<monsters_amount;k++){
 		centerx = monsters[k].i * 60 + 30;
 		centery = monsters[k].j * 60 + 30;
-		var img2 = new Image();
-		img2.src = "extensions/gamefigures/enemy1.png"
+		var img2 = new Image();		
+		img2.src = monsters_images[k];
 		context.drawImage(img2, centerx - 30, centery-30)
+	}
+	// draw moving score
+	if(moving_score!=null){
+		let centerxmoving = moving_score.i *60 +30;
+		let centerymoving = moving_score.j *60 +30;
+		var img3 = new Image();
+		img3.src = "extensions/gamefigures/moving.png"
+		context.drawImage(img3, centerxmoving-30, centerymoving-30);
 	}
 }
 
@@ -314,6 +325,21 @@ function UpdatePosition() {
 	if (board[shape.i][shape.j] == 1) {
 		score++;
 	}
+	if(moving_score!=null && shape.i == moving_score.i && shape.j == moving_score.j){
+		score+=50
+		moving_score = null;
+	}
+	if(moving_score!=null){
+		let random_move = Math.random();
+		if(random_move<0.25 && board[moving_score.i+1][moving_score.j]!=4 && moving_score.i<9) 
+			moving_score.i +=1;
+		else if(random_move<0.5 && board[moving_score.i-1][moving_score.j]!=4 && moving_score.i>0) 
+			moving_score.i -=1;
+		else if(random_move<0.75 && board[moving_score.i][moving_score.j+1]!=4 && moving_score.j<9) 
+			moving_score.j +=1;
+		else if(board[moving_score.i][moving_score.j-1]!=4 && moving_score.j>1) 
+			moving_score.j -=1;
+	}
 
 	for(let k=0;k<monsters_amount;k++){
 		if(shape.i == monsters[k].i && shape.j == monsters[k].j){
@@ -321,25 +347,30 @@ function UpdatePosition() {
 			life -= 1;
 			if (life == 0){
 				window.clearInterval(interval);
-				window.alert("Loser!");
-				score = 0
-				life = 5
+				window.alert("Loser!");				
+				score = 0;
+				life = 5;
 			}
-			Start();
+			remap();		
 		}
-	}
-	/*
-	if (board[shape.i][shape.j] == 3) {
-		score -= 10;
-		life -= 1;
-		if (life == 0){
-			window.clearInterval(interval);
-			window.alert("Loser!");
-			score = 0
-			life = 5
+		if(enemy_move_counter > 0){
+			 enemy_move_counter-=1;
+			 continue;
 		}
-		Start();
-	}*/
+		enemy_move_counter = 10;
+		if(shape.i > monsters[k].i && board[monsters[k].i+1][monsters[k].j] != 4){
+			monsters[k].i+=1;
+		}
+		else if(shape.i < monsters[k].i && board[monsters[k].i-1][monsters[k].j] != 4){
+			monsters[k].i-=1;
+		}
+		else if(shape.j > monsters[k].j && board[monsters[k].i][monsters[k].j+1] != 4){
+			monsters[k].j+=1;
+		}
+		else if(shape.j < monsters[k].j && board[monsters[k].i][monsters[k].j-1] != 4){
+			monsters[k].j-=1;
+		}
+	}	
 	board[shape.i][shape.j] = 2;
 	var currentTime = new Date();
 	time_elapsed = (currentTime - start_time) / 1000;	
